e8b55212de7e2ef5da6e5845d0ed3c03f65cbf15
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-28 16:59:25 -0700

    added update and used it to update info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,19 +5,30 @@ firebase.initializeApp(config.firebase);
 
 const database = firebase.database();
 
-// database.ref().set({
-//   name: 'Andrew Mead',
-//   age: 26,
-//   isSingle: false,
-//   location: {
-//     city: 'Philadelphia',
-//     country: 'United States'
-//   }
-// }).then(() => {
-//   console.log("Data is saved");
-// }).catch((e) => {
-//   console.log('This failed.', e);
-// });
+database.ref().set({
+  name: 'Andrew Mead',
+  age: 26,
+  isSingle: false,
+  stressLevel: 6,
+  job: {
+    title: 'Software Developer',
+    company: 'Google'
+  },
+  location: {
+    city: 'Philadelphia',
+    country: 'United States'
+  }
+}).then(() => {
+  console.log("Data is saved");
+}).catch((e) => {
+  console.log('This failed.', e);
+});
+
+database.ref().update({
+  stressLevel: 9,
+  'job/company': 'Amazon',
+  'location/city': 'Seattle'
+});
 
 // database.ref('isSingle')
 // .remove()
