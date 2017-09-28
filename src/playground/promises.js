dacc0b60c5284034db0c20172e0214b55726327d
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-28 13:06:01 -0700

    played with promises

---
+++ src/playground/promises.js
@@ -0,0 +1,14 @@
+const promise = new Promise((resolve, reject) => {
+  // resolve('This is my resolved data');
+  reject('Something went wrong!');
+}, 5000);
+
+console.log('before');
+
+promise.then((data) => {
+  console.log(data);
+}).catch((error)=>{
+  console.log('error: ', error);
+});
+
+console.log('after');
