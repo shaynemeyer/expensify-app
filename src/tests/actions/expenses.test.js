4027e2e8c84bb6a6c290a306ff85cc51c33d828e
Shayne Meyer <shayne.meyer@gmail.com>
2017-10-02 13:08:01 -0700

    added tests for startRemoveExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {
-  startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses
+  startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense
 } from "../../actions/expenses";
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
@@ -25,6 +25,24 @@ test('should setup remove expense action object', () => {
     id: '123abc'
   });
 });
+
+
+test('should remove expenses from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
+  });
+});
+
 
 test('should edit expense', () => {
   const action = editExpense('123abc', { note: 'New note value' });
